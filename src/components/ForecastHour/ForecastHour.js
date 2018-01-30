d9147ab38d0eede9d6d0c5260c76cdc5d30a9f2f
Zach Steiner <zsteiner@gmail.com>
2018-01-29 19:52:51 -0500

    Use Nicer Icon Set

--- src/components/ForecastHour/ForecastHour.js
+++ src/components/ForecastHour/ForecastHour.js
@@ -5,11 +5,15 @@ import moment from 'moment';
 import styles from './ForecastHour.scss';
 
 const ForecastHour = ({ forecast }) => {
+  const time = moment(forecast.dt*1000).format("h a");
   const conditions = forecast.weather[0].main;
+  const timeDay = moment(forecast.dt*1000).format("a");
+  const isDay = timeDay === 'am';
+    
   return (
     <li className={styles.forecastHour}>
-      <time className={styles.time}>{moment(forecast.dt*1000).format("h a")}</time>
-      <img src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`} className={styles.weatherIcon} alt={conditions} tooltip={conditions}/>
+      <time className={styles.time}>{time}</time>
+      <span className={`owf owf-${forecast.weather[0].id}${isDay ? '-d' : '-n'} ${styles.weatherIcon}`} alt={conditions} tooltip={conditions}></span>
       <h4 className={`temp ${styles.tempMain}`}>{Math.round(forecast.main.temp, 0)}</h4>
     </li>
   );
