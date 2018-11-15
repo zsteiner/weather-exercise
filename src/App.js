37be6afe32fd02c66faff90fc4fc37ca37f603e1
Zach Steiner <zsteiner@gmail.com>
2018-11-15 16:44:09 -0700

    Linting

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import CurrentWeather from './components/CurrentWeather/CurrentWeather';
-import Forecast from './components/Forecast/Forecast';
+import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
+import Forecast from "./components/Forecast/Forecast";
 
-import styles from './App.scss';
+import styles from "./App.scss";
 
-const apiKey = '321fd12b634b2f982e572096615c201a';
-const apiKeyGoogle = 'AIzaSyAVlJKciYbYy1oG2I-s2LcPVNl1qKJbC14';
+const apiKey = "321fd12b634b2f982e572096615c201a";
+const apiKeyGoogle = "AIzaSyAVlJKciYbYy1oG2I-s2LcPVNl1qKJbC14";
 
 class App extends Component {
-  
   constructor(props) {
     super(props);
-    
+
     this.state = {
       currentWeather: null,
-      city: '',
+      city: "",
       error: null,
       forecast: null,
       fetchingLocation: false,
       lat: 0,
       lon: 0,
-      units: 'imperial',
-      zipcode: '',
+      units: "imperial",
+      zipcode: ""
     };
   }
 
@@ -34,11 +33,11 @@ class App extends Component {
   getForecast = () => {
     this.getForecastData();
     this.getCurrentWeatherData();
-  }
-  
+  };
+
   getForecastData = () => {
     const { lat, lon, units, zipcode } = this.state;
-    
+
     const zipAPI = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},us&units=${units}&APPID=${apiKey}`;
     const coordinateAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&APPID=${apiKey}`;
     const api = lat && lon && !zipcode ? coordinateAPI : zipAPI;
@@ -46,24 +45,23 @@ class App extends Component {
     axios
       .get(api)
       .then(response => {
-
         this.setState({
           forecast: response.data.list,
           error: null
         });
       })
       .catch(err => {
-        console.log('Error happened during fetching!', err);
+        console.log("Error happened during fetching!", err);
         this.setState({
           forecast: null,
-          error: 'There was a problem getting forecast data. Try again.'
+          error: "There was a problem getting forecast data. Try again."
         });
       });
-  }
+  };
 
   getCurrentWeatherData = () => {
     const { lat, lon, units, zipcode } = this.state;
-    
+
     const zipAPI = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=${units}&APPID=${apiKey}`;
     const coordinateAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&APPID=${apiKey}`;
     const api = lat && lon && !zipcode ? coordinateAPI : zipAPI;
@@ -72,53 +70,55 @@ class App extends Component {
       .get(api)
       .then(response => {
         this.setState({
-         city: response.data.name,
-         currentWeather: response.data,
-         error: null
+          city: response.data.name,
+          currentWeather: response.data,
+          error: null
         });
       })
       .catch(err => {
-        console.log('Error happened during fetching!', err);
+        console.log("Error happened during fetching!", err);
         this.setState({
           currentWeather: null,
-          error: 'There was a problem getting current weather data. Try again.'
+          error: "There was a problem getting current weather data. Try again."
         });
       });
-  }
-  
-  updateZip = (event) => {
+  };
+
+  updateZip = event => {
     const zipcode = event.target.value;
 
     this.setState({
       zipcode: zipcode
     });
-    
-  }
-  
-  inputSubmit = (event) => {
+  };
+
+  inputSubmit = event => {
     if (event.which === 13 || event.keyCode === 13) {
       this.getForecast();
     }
-  }
+  };
 
   getLocation = () => {
     navigator.geolocation.getCurrentPosition(
-      (position) => {
+      position => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
 
-        this.setState({
-          lat: lat,
-          lon: lon,
-        }, this.lookUpLocation(lat, lon));
+        this.setState(
+          {
+            lat: lat,
+            lon: lon
+          },
+          this.lookUpLocation(lat, lon)
+        );
       },
-      (error) => alert(error.message),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+      error => alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
-  }
+  };
 
   lookUpLocation = (lat, lon) => {
-    const api = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${apiKeyGoogle}`
+    const api = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${apiKeyGoogle}`;
 
     this.setState({
       fetchingLocation: true
@@ -127,36 +127,88 @@ class App extends Component {
     axios
       .get(api)
       .then(response => {
-        this.setState({
-          zipcode: response.data.results[0].address_components[8].long_name,
-          fetchingLocation: false,
-          error: null
-        }, this.getForecast());
+        this.setState(
+          {
+            zipcode: response.data.results[0].address_components[8].long_name,
+            fetchingLocation: false,
+            error: null
+          },
+          this.getForecast()
+        );
       })
       .catch(err => {
         this.setState({
-          error: 'There was a problem looking up your zip code.'
+          error: "There was a problem looking up your zip code."
         });
-        console.log('Error happened during fetching!', err);
+        console.log("Error happened during fetching!", err);
       });
-  }  
-  
+  };
+
   render() {
-    
-    const { city, currentWeather, error, fetchingLocation, forecast, zipcode } = this.state;
+    const {
+      city,
+      currentWeather,
+      error,
+      fetchingLocation,
+      forecast,
+      zipcode
+    } = this.state;
 
     return (
       <div className={styles.app}>
         <header className={styles.header}>
-          { fetchingLocation ? <span className={`${styles.locationButton} loader`}>loading ... </span> : <button onClick={this.getLocation} className={styles.locationButton}><svg className="icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fillRule="evenodd" d="M8 5.09C6.393 5.09 5.09 6.394 5.09 8c0 1.607 1.303 2.91 2.91 2.91 1.607 0 2.91-1.303 2.91-2.91 0-1.607-1.303-2.91-2.91-2.91zm6.502 2.183c-.335-3.033-2.742-5.44-5.775-5.775V0H7.273v1.498c-3.033.335-5.44 2.742-5.775 5.775H0v1.454h1.498c.335 3.033 2.742 5.44 5.775 5.775V16h1.454v-1.498c3.033-.335 5.44-2.742 5.775-5.775H16V7.273h-1.498zM8 13.09c-2.815 0-5.09-2.276-5.09-5.091 0-2.815 2.275-5.09 5.09-5.09S13.09 5.184 13.09 8 10.816 13.09 8 13.09z"/></svg> Get my location</button> }     
-          <input type="text" placeholder='Enter Zip Code' onChange={this.updateZip} onKeyPress={this.inputSubmit} className={styles.zipcode} value={zipcode} />
-          <button onClick={this.getForecast} className={styles.submitButton}>Get My Forecast</button>     
-          {!error && city ? <h1 className={styles.city}>Forecast for {city}</h1> : null }
+          {fetchingLocation ? (
+            <span className={`${styles.locationButton} loader`}>
+              loading ...{" "}
+            </span>
+          ) : (
+            <button
+              onClick={this.getLocation}
+              className={styles.locationButton}
+            >
+              <svg
+                className="icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 5.09C6.393 5.09 5.09 6.394 5.09 8c0 1.607 1.303 2.91 2.91 2.91 1.607 0 2.91-1.303 2.91-2.91 0-1.607-1.303-2.91-2.91-2.91zm6.502 2.183c-.335-3.033-2.742-5.44-5.775-5.775V0H7.273v1.498c-3.033.335-5.44 2.742-5.775 5.775H0v1.454h1.498c.335 3.033 2.742 5.44 5.775 5.775V16h1.454v-1.498c3.033-.335 5.44-2.742 5.775-5.775H16V7.273h-1.498zM8 13.09c-2.815 0-5.09-2.276-5.09-5.091 0-2.815 2.275-5.09 5.09-5.09S13.09 5.184 13.09 8 10.816 13.09 8 13.09z"
+                />
+              </svg>{" "}
+              Get my location
+            </button>
+          )}
+          <input
+            type="text"
+            placeholder="Enter Zip Code"
+            onChange={this.updateZip}
+            onKeyPress={this.inputSubmit}
+            className={styles.zipcode}
+            value={zipcode}
+          />
+          <button onClick={this.getForecast} className={styles.submitButton}>
+            Get My Forecast
+          </button>
+          {!error && city ? (
+            <h1 className={styles.city}>Forecast for {city}</h1>
+          ) : null}
         </header>
-        { error || !currentWeather || !forecast ? <p className={styles.error}>{error}</p> : <div className={styles.forecastBody}>
-          {<CurrentWeather currentWeather={currentWeather} forecast={forecast}/>}
-          <Forecast forecast={forecast} />
-        </div> }
+        {error || !currentWeather || !forecast ? (
+          <p className={styles.error}>{error}</p>
+        ) : (
+          <div className={styles.forecastBody}>
+            {
+              <CurrentWeather
+                currentWeather={currentWeather}
+                forecast={forecast}
+              />
+            }
+            <Forecast forecast={forecast} />
+          </div>
+        )}
       </div>
     );
   }
--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,55 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import moment from 'moment';
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
 
-import { groupWeatherByDay } from '../../utils/groupWeatherByDay';
+import { groupWeatherByDay } from "../../utils/groupWeatherByDay";
 
-import styles from './CurrentWeather.scss';
+import styles from "./CurrentWeather.scss";
 
-import Date from '../Date/Date';
-import ForecastDay from '../ForecastDay/ForecastDay';
-import WeatherIcon from '../WeatherIcon/WeatherIcon';
+import Date from "../Date/Date";
+import ForecastDay from "../ForecastDay/ForecastDay";
+import WeatherIcon from "../WeatherIcon/WeatherIcon";
 
 const CurrentWeather = ({ currentWeather, forecast }) => {
-  
-  const currentTemp = currentWeather.main && Math.round(currentWeather.main.temp, 0);
+  const currentTemp =
+    currentWeather.main && Math.round(currentWeather.main.temp, 0);
   const conditions = currentWeather.weather && currentWeather.weather[0].main;
- 
+
   const today = moment().format("MMM D");
   const groupedByDay = groupWeatherByDay(forecast);
   const forecastToday = groupedByDay[today];
-  
+
   return (
     <aside className={styles.currentWeather}>
-      <Date 
-        className={styles.currentDate} 
-        date={currentWeather.dt}
-      />
+      <Date className={styles.currentDate} date={currentWeather.dt} />
       <section className={styles.currentWeatherDetails}>
-        <h2 className={`temp ${styles.currentTemp}`}>{ currentTemp }</h2>
+        <h2 className={`temp ${styles.currentTemp}`}>{currentTemp}</h2>
         <div className={styles.currentConditions}>
           <p className={styles.currentConditionsItem}>
-            <WeatherIcon 
+            <WeatherIcon
               className={styles.icon}
               date={currentWeather.dt}
               id={currentWeather.weather[0].id}
-              inline 
+              inline
             />
-            { conditions }
+            {conditions}
           </p>
           <p className={styles.currentConditionsItem}>
-            <WeatherIcon 
+            <WeatherIcon
               className={styles.icon}
               date={currentWeather.dt}
               id={956}
-              inline 
+              inline
             />
-            { `${currentWeather.wind.speed} mph`}
+            {`${currentWeather.wind.speed} mph`}
           </p>
         </div>
       </section>
-      {forecastToday ? <h3 className={styles.hourlyHeading}>Hourly Forecast</h3> : null }
-      {forecastToday ? <ForecastDay forecast={forecastToday} hideDate twoRow /> : null }
+      {forecastToday ? (
+        <h3 className={styles.hourlyHeading}>Hourly Forecast</h3>
+      ) : null}
+      {forecastToday ? (
+        <ForecastDay forecast={forecastToday} hideDate twoRow />
+      ) : null}
     </aside>
   );
 };
